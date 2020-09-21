76603e016a47dc104f0afdde82bae24f1b667c7d
Shelby <shelbyrobison14@gmail.com>
2020-09-21 16:40:56 -0400

    added function to get a random upper case letter

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+console.log(getRandomUpper());
