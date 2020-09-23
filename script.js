594b10bf919efb6e222f8d337fa022166de38e49
Shelby <shelbyrobison14@gmail.com>
2020-09-23 00:31:56 -0400

    updated array elements to strings - for password characters

--- script.js
+++ script.js
@@ -1,127 +1,58 @@
-//Create variables
+//Create variables for password characters
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var special = "!@#$^&%*()+=-[]{}|:<>?,.";
+var numbers = "1234567890";
+
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
-//Arrays for upper case letters, lower case letters, number, and symbols
-var randomUpper = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var randomLower = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var randomNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var randomSymbol = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  " < ",
-  "=",
-  " > ",
-  " ? ",
-  "@",
-  "[",
-  "]",
-  " ^ ",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-];
-
-// Write password to the #password input - code from class
-function writePassword() {
+// // Write password to the #password input - code from class
+function createPassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// Add event listener to generate button - code from class
+// // Add event listener to generate button - code from class
 generateBtn.addEventListener("click", createPassword);
 
-//Prompt user to choose how many characters they want their password
-function createPassword() {
-  characterLimit = prompt(
-    "How many characters would you like your password to be? Password length should be between 8 and 128 characters."
-  );
-  //alert user if their choice is outside of range or not a number
-  if (characterLimit < 8 || characterLimit > 128) {
-    alert("Password length should be between 8 and 128 characters");
-    createPassword();
-  }
-  //ask user if they would like to have uppercase letters
-  var createUpper = confirm(
-    "Would you like to have uppercase letters in your password?"
-  );
-  //ask user if they would like to have lowercase letters
-  var createLower = confirm(
-    "Would you like to have lowercase letters in your password?"
-  );
-  //ask users if they would like numbers in their password
-  var createNumbers = confirm("Would you like numbers in your password");
-  //ask user if they would like symbols in their password
-  var createSymbols = confirm("Would you like symbols in your password?");
-}
+// //Prompt user to choose how many characters they want their password
+// function createPassword() {
+//   characterLimit = prompt(
+//     "How many characters would you like your password to be? Password length should be between 8 and 128 characters."
+//   );
+//   //Alert user if their choice is outside of character range or not a number
+//   if (characterLimit < 8 || characterLimit > 128) {
+//     alert("Password length should be between 8 and 128 characters");
+//     createPassword();
+//   }
+//   //Ask user if they would like to have uppercase letters
+//   var createUpper = confirm(
+//     "Would you like to have uppercase letters in your password?"
+//   );
+//   //Ask user if they would like to have lowercase letters
+//   var createLower = confirm(
+//     "Would you like to have lowercase letters in your password?"
+//   );
+//   //Ask users if they would like numbers in their password
+//   var createNumber = confirm("Would you like numbers in your password");
+//   //Ask user if they would like symbols in their password
+//   var createSymbol = confirm("Would you like symbols in your password?");
+// }
+
+// //Conditions based on User Input
+// if (createUpper === yes) {
+//   possibleCharacters += randomUpper;
+// }
+// if (createLower) {
+//   possibleCharacters += randomLower;
+// }
+// if (createNumber) {
+//   possibleCharacters += randomNumber;
+// }
+// if (createSymbol) {
+//   possibleCharacters += randomSymbol;
+// } else if (createUpper && createLower && createNumber && createSymbol) {
+//   alert("Please include at least one value to generate your password");
+// }
