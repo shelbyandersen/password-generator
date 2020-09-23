f2e41fde9c62f524ee05fb86be7671e935577873
Shelby <shelbyrobison14@gmail.com>
2020-09-23 17:51:35 -0400

    reworked some of the JS code - reworked the for loop and used the concat method on the if statements

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Create variables
 //variables for password characters
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var special = "!@#$^&%*()+=-[]{}|:<>?,.";
-var numbers = "1234567890";
+var upper = ["A", "B", "C"];
+var lower = ["a", "b", "c"];
+var special = ["@", "$", "!"];
+var numbers = ["1", "2", "3"];
 
 //variables for possible characters and for password
-var possibleCharacters = "";
-var password = "";
+var possibleCharacters = [];
+var password = [];
 
 //variables for generate button and password text
 var generateBtn = document.querySelector("#generate");
@@ -15,14 +15,25 @@ var passwordText = document.querySelector("#password");
 
 //Prompt user to type how many characters they want their password
 function writePassword() {
-  characterLimit = prompt(
+  var characterLimit = prompt(
     "How many characters would you like your password to be? Password length should be between 8 and 128 characters."
   );
+  var parsedCharacterLimit = parseInt(characterLimit, 10);
+  console.log(parsedCharacterLimit);
+
   //Alert user if their choice is outside of character range or not a number
-  if (characterLimit < 8 || characterLimit > 128) {
-    alert("Password length should be between 8 and 128 characters");
-    writePassword();
+  if (
+    parsedCharacterLimit < 8 ||
+    parsedCharacterLimit > 128 ||
+    isNaN(parsedCharacterLimit)
+  ) {
+    alert("Please choose a number between 8 and 128.");
+    return "Try again";
+  } else {
+    console.log("Character Limit: ", characterLimit);
+    console.log("Character Limit Length: ", characterLimit.length);
   }
+
   //confirm if user wants uppercase letters in their password
   var includeUpper = confirm(
     "Would you like uppercase letters in your password?"
@@ -39,19 +50,20 @@ function writePassword() {
   var includeSymbols = confirm(
     "Would you like to include symbols in your password?"
   );
-
+  console.log("Include Upper: ", includeUpper);
   //User must choose at least one variable for password to generate - if user chooses more than 1, those variable types are added to password string
-  if (includeUpper) {
-    possibleCharacters + -upper;
+  if (includeUpper === true) {
+    possibleCharacters = possibleCharacters.concat(upper);
   }
   if (includeLower) {
-    possibleCharacters + -lower;
+    possibleCharacters = possibleCharacters.concat(lower);
   }
   if (includeNumbers) {
-    possibleCharacters + -numbers;
+    possibleCharacters = possibleCharacters.concat(numbers);
   }
   if (includeSymbols) {
-    possibleCharacters + -special;
+    possibleCharacters = possibleCharacters.concat(special);
+    console.log("Combined User Input: ", possibleCharacters);
   } else if (
     !includeUpper &&
     !includeLower &&
@@ -60,25 +72,33 @@ function writePassword() {
   ) {
     alert("Please include at least one variable to generate password");
   }
-  for (var i = 0; i < characterLimit; i++) {
-    password +=
-      possibleCharacters[
-        Math.floor(Math.random() == possibleCharacters.length)
-      ];
-  }
 
+  var randomCharacterValue = "";
+
+  for (var i = 0; i < parsedCharacterLimit; i++) {
+    randomCharacterValue =
+      possibleCharacters[Math.floor(possibleCharacters.length * Math.random())];
+    console.log("Random Character Value: ", randomCharacterValue);
+    password.push(randomCharacterValue);
+    // password.toString();
+    console.log("Password: ", password);
+  }
+  password.join(" ");
+  // return password;
+  // password.toString();
   //value for password - code from class
   passwordText.value = password;
 
-  console.log(password);
+  //   console.log(password);
 }
 
 // Add event listener to generate password - code from class
 generateBtn.addEventListener("click", writePassword);
-function getRandomCharacters() {
-  return possibleCharacters[
-    (Math.floor(
-      Math.random() + possibleCharacters.length
-    ) = possibleCharacters.length)
-  ];
-}
+
+// function getRandomCharacters() {
+//   return possibleCharacters[
+//     (Math.floor(
+//       Math.random() * possibleCharacters.length
+//     ) = possibleCharacters.length)
+//   ];
+// }
