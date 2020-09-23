435ec13cd3c81fdfb3ca1f2a34768268a5a55eb2
Shelby <shelbyrobison14@gmail.com>
2020-09-23 09:01:07 -0400

    changed my array of letters, numbers, and symbols to variables

--- script.js
+++ script.js
@@ -1,58 +1,38 @@
-//Create variables for password characters
+//Create variables
+//variables for password characters
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var special = "!@#$^&%*()+=-[]{}|:<>?,.";
 var numbers = "1234567890";
 
+//variables for possible characters and for password
+var possibleCharacters = "";
+var password = "";
+
+//variables for generate button and password text
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
-// // Write password to the #password input - code from class
-function createPassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+//Prompt user to write how many characters they want their password
+function writePassword() {
+  characterLimit = prompt(
+    "How many characters would you like your password to be? Password length should be between 8 and 128 characters."
+  );
+  //Alert user if their choice is outside of character range or not a number
+  if (characterLimit < 8 || characterLimit > 128) {
+    alert("Password length should be between 8 and 128 characters");
+    writePassword();
+  }
+  var includeUpper = confirm(
+    "Would you like uppercase letters in your password?"
+  );
+  var includeLower = confirm(
+    "Would you like lowercase letters in your password?"
+  );
+  var includeNumbers = confirm(
+    "Would you like to include numbers in your password?"
+  );
+  var includeSymbols = confirm(
+    "Would you like to include symbols in your password?"
+  );
 }
-
-// // Add event listener to generate button - code from class
-generateBtn.addEventListener("click", createPassword);
-
-// //Prompt user to choose how many characters they want their password
-// function createPassword() {
-//   characterLimit = prompt(
-//     "How many characters would you like your password to be? Password length should be between 8 and 128 characters."
-//   );
-//   //Alert user if their choice is outside of character range or not a number
-//   if (characterLimit < 8 || characterLimit > 128) {
-//     alert("Password length should be between 8 and 128 characters");
-//     createPassword();
-//   }
-//   //Ask user if they would like to have uppercase letters
-//   var createUpper = confirm(
-//     "Would you like to have uppercase letters in your password?"
-//   );
-//   //Ask user if they would like to have lowercase letters
-//   var createLower = confirm(
-//     "Would you like to have lowercase letters in your password?"
-//   );
-//   //Ask users if they would like numbers in their password
-//   var createNumber = confirm("Would you like numbers in your password");
-//   //Ask user if they would like symbols in their password
-//   var createSymbol = confirm("Would you like symbols in your password?");
-// }
-
-// //Conditions based on User Input
-// if (createUpper === yes) {
-//   possibleCharacters += randomUpper;
-// }
-// if (createLower) {
-//   possibleCharacters += randomLower;
-// }
-// if (createNumber) {
-//   possibleCharacters += randomNumber;
-// }
-// if (createSymbol) {
-//   possibleCharacters += randomSymbol;
-// } else if (createUpper && createLower && createNumber && createSymbol) {
-//   alert("Please include at least one value to generate your password");
-// }
