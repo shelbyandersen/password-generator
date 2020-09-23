4c3e97703ae67a5145c620bc7255e787619475af
Shelby <shelbyrobison14@gmail.com>
2020-09-22 23:23:50 -0400

    created function to ask user for their input with length, characters, numbers, and symbols

--- script.js
+++ script.js
@@ -90,3 +90,27 @@ var randomSymbol = [
   "}",
   "~",
 ];
+
+//Prompt user to choose how many characters they want their password
+function createPassword() {
+  characterLimit = prompt(
+    "How many characters would you like your password to be? Password length should be between 8 and 128 characters."
+  );
+  //alert user if their choice is outside of range or not a number
+  if (characterLimit < 8 || characterLimit > 128) {
+    alert("Password length should be between 8 and 128 characters");
+    createPassword();
+  }
+  //ask user if they would like to have uppercase letters
+  var createUpper = confirm(
+    "Would you like to have uppercase letters in your password?"
+  );
+  //ask user if they would like to have lowercase letters
+  var createLower = confirm(
+    "Would you like to have lowercase letters in your password?"
+  );
+  // //ask users if they would like numbers in their password
+  // var createNumbers = confirm("Would you like numbers in your password");
+  // //ask user if they would like symbols in their password
+  // var createSymbols = confirm("Would you like symbols in your password?");
+}
